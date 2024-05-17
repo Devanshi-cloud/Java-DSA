41cd8a3ac7d81ae5f9a300d8656e5abf37d814d5
Devanshi Jaiswal <devanshishs@gmail.com>
2024-05-17 14:29:05 +0530

    Add files via upload

---
+++ Java-DSA/Stars.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Stars {
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string with *");
+        String str = sc.nextLine();
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < str.length(); i++)
+        {
+            char c = str.charAt(i);
+            if(c != '*'){
+                res.append(c);
+            }
+            else if(i > 0 && c == '*') {
+                res.deleteCharAt((res.length()) - 1);
+            }
+        }
+        System.out.print(res.toString());
+    }
+}
